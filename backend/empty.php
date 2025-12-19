f042af9219714f7d8b4a106f8e1ce8cbe80d7c39
omeshchintha <omesh.chintha30@gmail.com>
2025-12-19 11:57:10 +0530

    ui updated

--- backend/empty.php
+++ backend/empty.php
@@ -5,6 +5,7 @@ if (isset($_GET['cors'])) {
     header('Access-Control-Allow-Origin: *');
     header('Access-Control-Allow-Methods: GET, POST');
     header('Access-Control-Allow-Headers: Content-Encoding, Content-Type');
+        // header('Access-Control-Allow-Headers: Content-Encoding, Content-Type');
 }
 
 header('Cache-Control: no-store, no-cache, must-revalidate, max-age=0, s-maxage=0');
